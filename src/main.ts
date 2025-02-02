7568cff27fa64750266bc1b0e576ad22cb79dfe4
ahmadabuhussen <ahmad@simplify9.com>
2025-02-02 19:25:21 +0300

    generate moch data

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MockDataService } from './mock-data/mock-data.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,9 +9,14 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',  // Allows all standard headers
     credentials: false,  // Disables credentials (cookies or authorization headers)
   });
+  //todo generate mock data
+  // const mockDataService = app.get(MockDataService);
+  // await mockDataService.generateMockData();
+
   // await app.close();
   await app.listen(4000);
 }
+
 bootstrap();
 // docker-compose up --build
 
